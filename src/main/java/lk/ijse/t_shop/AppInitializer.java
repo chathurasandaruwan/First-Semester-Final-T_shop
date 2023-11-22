e839a750e3724c862c348de573fca7f4eb66cc3e
chathura <chathurasandaruwan122@gmail.com>
2023-11-22 21:00:14 +0530

    second commit

--- src/main/java/lk/ijse/t_shop/AppInitializer.java
+++ src/main/java/lk/ijse/t_shop/AppInitializer.java
@@ -1,9 +1,15 @@
 package lk.ijse.t_shop;
 
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
+import javafx.util.Duration;
+
+import java.io.IOException;
 
 public class AppInitializer extends Application {
     public static void main(String[] args) {
@@ -12,10 +18,53 @@ public class AppInitializer extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        stage.setScene(new Scene(FXMLLoader.load(this.getClass().getResource("/view/login_form.fxml"))));
+        AnchorPane rootNode = FXMLLoader.load(this.getClass().getResource("/view/loading_form.fxml"));
+        Scene scene = new Scene(rootNode);
+        stage.setScene(scene);
+        stage.setTitle("LOADING .............");
         stage.centerOnScreen();
-        stage.setTitle("Logging Page");
 
+        //show scene to the crowd
         stage.show();
+
+        Timeline timeline = new Timeline(new KeyFrame(
+                Duration.seconds(10),
+                event -> {
+                    try {
+                        closeApplication(rootNode,stage);
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+        ));
+        timeline.play();
+    }
+
+    private void closeApplication(AnchorPane rootNode,Stage stage) throws IOException {
+        rootNode = FXMLLoader.load(this.getClass().getResource("/view/login_form.fxml"));
+        Scene scene = new Scene(rootNode);
+        stage.setScene(scene);
+        stage.setTitle("Logging Page");
+        stage.centerOnScreen();
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+ stage.setScene(new Scene(FXMLLoader.load(this.getClass().getResource("/view/login_form.fxml"))));
+         stage.centerOnScreen();
+         stage.setTitle("Logging Page");
+         stage.show();*/
---
+++ src/main/java/lk/ijse/t_shop/controller/loadingFromController.java
@@ -0,0 +1,34 @@
+package lk.ijse.t_shop.controller;
+
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.util.Duration;
+
+public class loadingFromController {
+    @FXML
+    private Label lblChange;
+    private final String[] textValues = {"Please wait.........","Check Licence Key........","Check Database Instance........"};
+    private int currentIndex = 0;
+
+    public void initialize(){
+        setLblChange();
+    }
+    public void setLblChange(){
+    Timeline timeline = new Timeline(new KeyFrame(
+            Duration.seconds(3),
+            event -> updateLabel()
+    ));
+        timeline.setCycleCount(Timeline.INDEFINITE); // Repeat indefinitely
+        timeline.play();
+}
+
+    private void updateLabel() {
+        lblChange.setText(textValues[currentIndex]);
+        currentIndex = (currentIndex + 1) % textValues.length;
+
+
+        //lblChange.setText("Check Licence Key: " + System.currentTimeMillis());
+    }
+}
