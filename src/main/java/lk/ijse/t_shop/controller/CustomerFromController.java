d29c81795623f12a05cbf0a4ec11ec4b4cffe073
chathura <chathurasandaruwan122@gmail.com>
2023-11-23 20:52:11 +0530

    forth commit

--- src/main/java/lk/ijse/t_shop/controller/CustomerFromController.java
+++ src/main/java/lk/ijse/t_shop/controller/CustomerFromController.java
@@ -132,7 +132,7 @@ public class CustomerFromController {
             new Alert(Alert.AlertType.ERROR,"Invalid Address !!").show();
             return false;
         }
-        boolean contactNo =Pattern.matches("[0-9]{10,}",dto.getContacNo());
+        boolean contactNo =Pattern.matches("^(?:0|94|\\+94|0094)?(?:(11|21|23|24|25|26|27|31|32|33|34|35|36|37|38|41|45|47|51|52|54|55|57|63|65|66|67|81|91)(0|2|3|4|5|7|9)|7(0|1|2|4|5|6|7|8)\\d)\\d{6}$",dto.getContacNo());
         if (!contactNo){
             new Alert(Alert.AlertType.ERROR,"Invalid Contact No !!").show();
             return false;
--- src/main/java/lk/ijse/t_shop/controller/SupplierFromController.java
+++ src/main/java/lk/ijse/t_shop/controller/SupplierFromController.java
@@ -136,7 +136,7 @@ public class SupplierFromController {
             new Alert(Alert.AlertType.ERROR,"Invalid Contact No !!").show();
             return false;
         }
-        boolean contactNo =Pattern.matches("[0-9]{10,}",textContactNo.getText());
+        boolean contactNo =Pattern.matches("^(?:0|94|\\+94|0094)?(?:(11|21|23|24|25|26|27|31|32|33|34|35|36|37|38|41|45|47|51|52|54|55|57|63|65|66|67|81|91)(0|2|3|4|5|7|9)|7(0|1|2|4|5|6|7|8)\\d)\\d{6}$",textContactNo.getText());
         if (!contactNo){
             new Alert(Alert.AlertType.ERROR,"Invalid Contact No !!").show();
             return false;
